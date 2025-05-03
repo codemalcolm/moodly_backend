cd676dde55d0a53445e0762fb8df8d1ee52a6cb4
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-03 16:46:48 +0200

    Added basic createEntry request body handling

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,21 +1,33 @@
 const StatusCodes = require("http-status-codes");
+const JournalEntry = require("../models/JournalEntry");
+const { BadRequestError } = require("../errors/index");
 
-const getEntries = async (req,res) =>{
-    res.status(StatusCodes.OK).json({message : "getEntries visited"})
-}
+const getEntries = async (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "getEntries visited" });
+};
 
-const createEntry = async (req,res) =>{
-    res.status(StatusCodes.OK).json({message : "createEntry visited"})
-}
+const createEntry = async (req, res) => {
+  // TODO - implement image upload
 
-const updateEntry = async (req,res) =>{
-    res.status(StatusCodes.OK).json({message : "updateEntry visited"})
-}
+  const { entryText, entryDateAndTime } = req.body;
 
-const deleteEntry = async (req,res) =>{
-    res.status(StatusCodes.OK).json({message : "deleteEntry visited"})
-}
+  if (!entryText || !entryDateAndTime) {
+    throw new BadRequestError(
+      "Please provide Journal Entry Text and entryDateAndTime"
+    );
+  }
 
+  const journalEntry = await JournalEntry.create({ ...req.body });
 
+  res.status(StatusCodes.OK).json({ journalEntry });
+};
 
-module.exports = { getEntries, createEntry, updateEntry, deleteEntry }
+const updateEntry = async (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "updateEntry visited" });
+};
+
+const deleteEntry = async (req, res) => {
+  res.status(StatusCodes.OK).json({ message: "deleteEntry visited" });
+};
+
+module.exports = { getEntries, createEntry, updateEntry, deleteEntry };
