3b20886b7b820b2c42b4784b486d0a34b60e7c44
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-03 13:08:26 +0200

    getDay by id implemented

--- controllers/days.js
+++ controllers/days.js
@@ -2,15 +2,21 @@ const StatusCodes = require("http-status-codes");
 const DayEntry = require("../models/DayEntry");
 
 const getDay = async (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "getSingleDayRoute visited" });
+  const { dayId } = req.params;
+  const dayEntry = await DayEntry.findOne({ _id: dayId });
+
+  if(!dayEntry){
+    throw new Error(`Day with id : ${dayId} not found`);
+  }
+
+  res.status(StatusCodes.OK).json({ dayEntry });
 };
 
 const createDay = async (req, res) => {
-  
   const { entryDate } = req.body;
   if (!entryDate) throw new Error("entryDate not provided");
 
-  const dayEntry = await DayEntry.create({ ...req.body })
+  const dayEntry = await DayEntry.create({ ...req.body });
 
   res.status(StatusCodes.OK).json({ dayEntry });
 };
