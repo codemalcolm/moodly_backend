b8e0cc306c71481b1004792a064439af31816e24
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-03 12:49:51 +0200

    (server) createDay controller handling request body

--- controllers/days.js
+++ controllers/days.js
@@ -1,17 +1,22 @@
 const StatusCodes = require("http-status-codes");
+const DayEntry = require("../models/DayEntry");
 
 const getDay = async (req, res) => {
   res.status(StatusCodes.OK).json({ message: "getSingleDayRoute visited" });
 };
 
 const createDay = async (req, res) => {
-  res.status(StatusCodes.OK).json({ message: "createDay visited" });
+  
+  const { entryDate } = req.body;
+  if (!entryDate) throw new Error("entryDate not provided");
+
+  const dayEntry = await DayEntry.create({ ...req.body })
+
+  res.status(StatusCodes.OK).json({ dayEntry });
 };
 
 const updateDay = async (req, res) => {
   res.status(StatusCodes.OK).json({ message: "updateDay visited" });
 };
-
-
 
 module.exports = { getDay, createDay, updateDay };
