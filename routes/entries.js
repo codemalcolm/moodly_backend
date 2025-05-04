cc301bfb3d87ef857e40643b693102a6f63bc5b0
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-04 14:06:48 +0200

    Remade routes for controllers requirying  journalEntries id

--- routes/entries.js
+++ routes/entries.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { getEntries, createEntry, updateEntry, deleteEntry } = require("../controllers/entries");
 
-router.route("/").get(getEntries).post(createEntry).put(updateEntry).delete(deleteEntry);
+router.route("/").get(getEntries).post(createEntry);
+router.route("/:journalEntryId").put(updateEntry).delete(deleteEntry);
 
 module.exports = router;
