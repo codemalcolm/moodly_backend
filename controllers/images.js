ba5e8d7e0ca6f4a81fe23b90fe46bf04139ec2cf
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-05 21:18:29 +0200

    🖼️ (images) Created controller for uploading a photo

--- controllers/images.js
+++ controllers/images.js
@@ -1,5 +1,35 @@
+const StatusCodes = require("http-status-codes");
+const JournalEntry = require("../models/JournalEntry")
+const SingleImage = require("../models/SingleImage")
+
+
 const uploadPhoto = async (req, res) => {
-    res.status(200).json({message:"images controller !!"})
+  const { journalEntryId } = req.body;
+
+  if (!journalEntryId) {
+    throw new BadRequestError("Journal entry id missing");
+  }
+
+  const journalEntry = await JournalEntry.findById({ _id: journalEntryId });
+
+  if (!journalEntry) {
+    throw new NotFoundError("Journal entry not found");
+  }
+
+  const image = await SingleImage.create({
+    journalEntryId: journalEntryId,
+    imageData: req.file.buffer,
+    imageType: req.file.mimetype,
+  });
+
+  journalEntry.images.push(image._id);
+  await journalEntry.save();
+
+  res.status(StatusCodes.OK).json({
+    message: "Uploaded successfully",
+    file: image,
+    updatedJournalEntry: journalEntry,
+  });
 };
 
 module.exports = { uploadPhoto };
