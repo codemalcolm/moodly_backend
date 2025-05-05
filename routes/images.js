2c4b4629af463dd6593ab8b712c439dceffbebb9
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-05 21:16:51 +0200

    🖼️ (images) Created route with multer mw for uploading a photo

--- routes/images.js
+++ routes/images.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
-// add controllers
-const {uploadPhoto} = require("../controllers/images.js")
+const { uploadPhoto } = require("../controllers/images.js");
 
-router.route("/").post(uploadPhoto)
+const storage = multer.memoryStorage();
 
+const upload = multer({ storage: storage });
+
+router.route("/").post(upload.single("file"), uploadPhoto);
 
 module.exports = router;
