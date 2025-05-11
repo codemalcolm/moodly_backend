8e270fcb6d9a866687afbba4ace8e307eef50a90
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-11 10:39:29 +0200

    Added pagination middleware into entries routes

--- routes/entries.js
+++ routes/entries.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const BadRequestError = require("../errors/bad-request.js");
+const paginatedResults = require("../middleware/paginated-results.js");
 
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
@@ -25,6 +26,7 @@ const {
   createEntry,
   updateEntry,
   deleteEntry,
+  getEveryJournalEntry,
 } = require("../controllers/entries");
 
 const {
@@ -33,16 +35,18 @@ const {
   deletePhoto,
   uploadMultiplePhotos,
 } = require("../controllers/images.js");
+const JournalEntry = require("../models/JournalEntry.js");
 
-router.route("/").post(createEntry);
 router
-  .route("/:journalEntryId")
-  .put(updateEntry)
-  .delete(deleteEntry)
+  .route("/")
+  .post(createEntry)
+  .get(paginatedResults(JournalEntry), getEveryJournalEntry);
+router.route("/:journalEntryId").put(updateEntry).delete(deleteEntry);
 
 router
   .route("/:journalEntryId/images")
-  .post(upload.array("file", 3), uploadMultiplePhotos).get(fetchPhotosFromJournalEntryId);
+  .post(upload.array("file", 3), uploadMultiplePhotos)
+  .get(fetchPhotosFromJournalEntryId);
 
 router.route("/:journalEntryId/images/:imageId").delete(deletePhoto);
 
