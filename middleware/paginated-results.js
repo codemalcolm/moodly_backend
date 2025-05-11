10943d06133aa05e13dd2b00c4b41d24ceac0d21
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-11 10:38:27 +0200

    Created pagination middleware

---
+++ middleware/paginated-results.js
@@ -0,0 +1,36 @@
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    console.log(page, limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < await model.countDocuments().exec()) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.prev = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+    } catch (error) {
+      throw new Error(error);
+    }
+
+    res.paginatedResults = results;
+    next();
+  };
+}
+
+module.exports = paginatedResults;
