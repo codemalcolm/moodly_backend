d37ea3d3131991969111090073f31261a29f7113
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-17 14:34:26 +0200

    Moved dailyTask controllers to days route

--- routes/days.js
+++ routes/days.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { getDay, createDay, updateDay, getDayByDate,getJournalEntries } = require("../controllers/days");
+const { createDailyTask,updateDailyTask,deleteDailyTask } = require("../controllers/daily-tasks");
 
 router.route("/").post(createDay).get(getDayByDate);
 router.route("/:dayId").patch(updateDay);
 router.route("/:dayId/entries").get(getJournalEntries);
+router.route("/:dayId/daily-tasks").post(createDailyTask);
+router.route("/:dayId/daily-tasks/:dailyTaskId").get(createDailyTask);
 
 module.exports = router;
