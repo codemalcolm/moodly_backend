d590597ed1d1bcbbcc111b25244c9b95cffbd921
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-17 15:11:04 +0200

    Created controller for creating a daily task

--- controllers/daily-tasks.js
+++ controllers/daily-tasks.js
@@ -1,8 +1,30 @@
+const StatusCodes = require("http-status-codes");
+const DailyTask = require("../models/DailyTask");
+const DayEntry = require("../models/DayEntry")
+const { BadRequestError } = require("../errors");
+
 const createDailyTask = async (req, res) => {
-  res.send("OK");
+  const { name } = req.body;
+  const { dayId } = req.params;
+
+  if (!name && !dayId) {
+    BadRequestError("Name of daily task missing");
+  }
+
+  const dailyTask = await DailyTask.create({ ...req.body });
+
+  const updatedDayEntry = await DayEntry.findOneAndUpdate(
+    { _id: dayId },
+    { $push: { dailyTasks: dailyTask._id } },
+    { new: true }
+  );
+
+  res.status(StatusCodes.OK).json({ updatedDayEntry });
 };
+
 const updateDailyTask = async (req, res) => {
   const { dailyTaskId } = req.params;
+  const { name, isDone } = req.body;
   res.send(dailyTaskId);
 };
 const deleteDailyTask = async (req, res) => {
