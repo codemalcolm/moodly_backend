eb14b6fd9ea7a3d91ee3ef107b4cc84f69ce196f
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-18 13:05:21 +0200

    Implemented updating daily task

--- controllers/daily-tasks.js
+++ controllers/daily-tasks.js
@@ -1,11 +1,13 @@
 const StatusCodes = require("http-status-codes");
 const DailyTask = require("../models/DailyTask");
-const DayEntry = require("../models/DayEntry")
+const DayEntry = require("../models/DayEntry");
 const { BadRequestError } = require("../errors");
 
 const createDailyTask = async (req, res) => {
   const { name } = req.body;
   const { dayId } = req.params;
+
+  console.log(name, dayId);
 
   if (!name && !dayId) {
     BadRequestError("Name of daily task missing");
@@ -23,9 +25,26 @@ const createDailyTask = async (req, res) => {
 };
 
 const updateDailyTask = async (req, res) => {
-  const { dailyTaskId } = req.params;
+  const { dailyTaskId, dayId } = req.params;
   const { name, isDone } = req.body;
-  res.send(dailyTaskId);
+
+  if (!dailyTaskId && !dayId) {
+    BadRequestError("Day id or task id missing");
+  }
+
+  if (!name && !isDone) {
+    BadRequestError("Name of daily task missing");
+  }
+
+  console.log(req.body);
+
+  const updatedDailyTask = await DailyTask.findByIdAndUpdate(
+    { _id : dailyTaskId },
+    { ...req.body },
+    { runValidators: true, new: true }
+  );
+
+  res.status(StatusCodes.OK).json({ updatedDailyTask });
 };
 const deleteDailyTask = async (req, res) => {
   const { dailyTaskId } = req.params;
