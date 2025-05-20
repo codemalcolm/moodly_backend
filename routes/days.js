5c5a619fd52bdf0e8ce882f278d8c33e8a89d52d
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-20 23:06:31 +0200

    changed paginatedResults population settings

--- routes/days.js
+++ routes/days.js
@@ -18,12 +18,24 @@ const {
 const paginatedResults = require("../middleware/paginated-results.js");
 
 router.route("/").post(createDay).get(getDayByDate);
-router
-  .route("/all")
-  .get(paginatedResults(DayEntry, ["journalEntries"]), getAllDayEntries);
+router.route("/all").get(
+  paginatedResults(DayEntry, [
+    {
+      path: "journalEntries",
+      populate: {
+        path: "images",
+        model: "SingleImage", 
+      },
+    },
+  ]),
+  getAllDayEntries
+);
 router.route("/:dayId").patch(updateDay).get(getDay);
 router.route("/:dayId/entries").get(getJournalEntries);
 router.route("/:dayId/daily-tasks").post(createDailyTask);
-router.route("/:dayId/daily-tasks/:dailyTaskId").patch(updateDailyTask).delete(deleteDailyTask);
+router
+  .route("/:dayId/daily-tasks/:dailyTaskId")
+  .patch(updateDailyTask)
+  .delete(deleteDailyTask);
 
 module.exports = router;
