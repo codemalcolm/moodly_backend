ff2e96fef4f64523a417ef37c9aec8d5c4652518
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-05-22 12:26:41 +0200

    delete photo controller

--- routes/images.js
+++ routes/images.js
@@ -5,6 +5,7 @@ const multer = require("multer");
 const {
   uploadPhoto,
   fetchPhotosFromJournalEntryId,
+  deletePhoto
 } = require("../controllers/images.js");
 
 const storage = multer.memoryStorage();
@@ -14,5 +15,6 @@ const upload = multer({ storage: storage });
 router.route("/").post(upload.single("file"), uploadPhoto);
 
 router.route("/:journalEntryId").get(fetchPhotosFromJournalEntryId);
+router.route("/:imageId").delete(deletePhoto)
 
 module.exports = router;
